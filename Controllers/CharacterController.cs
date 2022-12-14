268e74bedb24100b2993da6f3fd0fe13072340e8
Daniel Saes <dr_saes@icloud.com>
2022-12-14 20:14:06 -0300

    MySql implementation

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("GetAll")]
+        public async  Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> GetCharacters(){
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingleCharacter(int id){
+            
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async  Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
+        {
+           
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
+
+        [HttpPut]
+        public async  Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> UpdateCharacter(UpdateCharacterDto updateCharacter)
+        {
+
+            var response = await _characterService.UpdateCharacter(updateCharacter);
+            if(response.Data == null){
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> DeleteCharacter(int id){
+            
+            var response = await _characterService.DeleteCharacter(id);
+            if(response.Data == null){
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
+global using dotnet_rpg.Models;
+global using dotnet_rpg.Services.CharacterServices; 
+global using dotnet_rpg.Dtos.Character;
+global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DataContext>(opts =>
+opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
